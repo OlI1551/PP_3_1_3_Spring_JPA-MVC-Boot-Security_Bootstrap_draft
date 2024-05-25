34bf7ccab399d7f20189cd4403d70bb4b9f1d3e6
Olga Basheva <I15510000000@gmail.com>
2024-05-25 10:59:32 +0800

    some amendments 1

--- src/main/java/ru/kata/spring/boot_bootstrap/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_bootstrap/demo/services/UserServiceImpl.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_bootstrap.demo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
